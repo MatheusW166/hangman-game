796df8217e2b9a571232ecdbb3796535d185a58e
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-02-15 12:53:17 -0300

    Word component

---
+++ src/components/Word.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const WordButton = styled.button(({ disabled, theme }) => ({
+  width: "40px",
+  height: "40px",
+  borderRadius: "3px",
+  border: `1px solid ${theme.wordBtnBorder}`,
+  color: disabled ? theme.wordBtnDisabledText : theme.wordBtnText,
+  background: disabled ? theme.wordBtnDisabledBg : theme.wordBtnBg,
+  fontWeight: "700",
+  cursor: !disabled && "pointer",
+  transition: "background 0.3s ease",
+  ":hover": !disabled && {
+    background: "#c3d6e3",
+  },
+  ":active": !disabled && {
+    background: "#b2c6d4",
+  },
+}));
+
+export default function Word({ children, onClick, disabled = true }) {
+  if (typeof children !== "string") return "";
+  return (
+    <WordButton disabled={disabled} onClick={onClick}>
+      {children.toUpperCase()}
+    </WordButton>
+  );
+}
